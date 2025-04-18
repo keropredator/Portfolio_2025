3ff294c5fe719c3a718e760cbba6e23df550a04e
keropredator <kero.top.gogo2014@gmail.com>
2025-04-18 23:16:30 +0200

    Fix typo in reset button label

--- Grid_Play/src/pages/TicTacToe.tsx
+++ Grid_Play/src/pages/TicTacToe.tsx
@@ -107,7 +107,7 @@ function TicTacToe() {
           ))}
         </div>
         <button className="reset-button" onClick={handleReset}>
-          Reset Game
+          Reset Gam
         </button>
       </div>
     </div>
