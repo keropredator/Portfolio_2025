bac262a05f60e287b5682b3a025a876777469a02
keropredator <kero.top.gogo2014@gmail.com>
2025-04-18 22:24:47 +0200

    new update

--- Grid_Play/src/pages/TicTacToe.tsx
+++ Grid_Play/src/pages/TicTacToe.tsx
@@ -1,14 +1,54 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useState, useEffect, useCallback } from 'react';
 import './TicTacToe.css';
 
 function TicTacToe() {
   const [cells, setCells] = useState(Array(9).fill(null));
+  const [currentPlayer, setCurrentPlayer] = useState<'X' | 'O'>('X');
+  const [gameOver, setGameOver] = useState(false);
+
+  const winningCombination = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  const checkForWinner = () => {
+    for (const combination of winningCombination) {
+      const [a, b, c] = combination;
+      if (cells[a] && cells[a] === cells[b] && cells[a] === cells[c]) {
+        setGameOver(true);
+        return cells[a];
+      }
+    }
+    if (!cells.includes(null)) {
+      setGameOver(true);
+      return 'Draw';
+    }
+    return null;
+  };
 
   const handleClick = (index: number) => {
+    if (cells[index] !== null || gameOver) return;
+
     const newCells = [...cells];
-    if (newCells[index] === null) {
-      newCells[index] = 'X';
-      setCells(newCells);
+    newCells[index] = currentPlayer;
+    setCells(newCells);
+
+    const result = checkForWinner();
+    if (result) {
+      if (result === 'draw') {
+        alert("It's a Draw");
+      } else {
+        alert(`Player ${result} Win!`);
+      }
+    } else {
+      setCurrentPlayer(currentPlayer === 'X' ? 'O' : 'X');
     }
   };
 
@@ -32,7 +72,9 @@ function TicTacToe() {
         handleClick(index);
       }
     },
-    [cells]
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [cells, currentPlayer, gameOver]
+    //---------------------------------**-------------------------------**--------
   );
 
   useEffect(() => {
@@ -42,21 +84,31 @@ function TicTacToe() {
     };
   }, [handleKeyPress]);
 
+  const handleReset = () => {
+    setCells(Array(9).fill(null));
+    setCurrentPlayer('X');
+    setGameOver(false);
+  };
+
   return (
     <div className="game-wrapper">
       <div className="game-container">
         <h1>Tic Tac Toe</h1>
+        <h2>Current Player : {currentPlayer} </h2>
         <div className="board">
           {cells.map((cell, index) => (
             <div
               key={index}
-              className="cell"
+              className={`cell ${cell === 'X' ? 'x' : cell === 'O' ? 'o' : ''}`}
               onClick={() => handleClick(index)}
             >
               {cell}
             </div>
           ))}
         </div>
+        <button className="reset-button" onClick={handleReset}>
+          Reset Game
+        </button>
       </div>
     </div>
   );
